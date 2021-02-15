cc8fd73acccd49a801947564d38f56ec1201ad4c
zuckpa1999 <zuckpa1999@gmail.com>
2021-02-15 23:12:43 +0700

    implement full CRUD

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+const methodOverride = require('method-override')
 const Campground = require('./models/campground')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -25,6 +26,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.get('/', (req, res) => {
 	res.render('home')
 })
@@ -53,6 +55,24 @@ app.get('/campgrounds/:id', async (req, res) => {
 
 	res.render('campgrounds/show', { campground })
 
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+	const campground = await Campground.findById(req.params.id)
+
+	res.render('campgrounds/edit', { campground })
+
+})
+app.put('/campgrounds/:id', async (req, res) => {
+	const { id } = req.params;
+	const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+	res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+	const { id } = req.params;
+	await Campground.findByIdAndDelete(id)
+	res.redirect('/campgrounds')
 })
 
 
